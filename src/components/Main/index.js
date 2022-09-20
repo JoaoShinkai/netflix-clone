001dabb0792734f6fa92cebf89c0369db1a09a4f
unknown <joaogabriel1412@gmail.com>
2022-09-20 20:12:22 -0300

    create new categories

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './style.css'
 
 export default function Main(props){
@@ -8,7 +9,14 @@ export default function Main(props){
         <div className='movie-main'>
             <img src={`https://image.tmdb.org/t/p/original/${props.movie.backdrop_path}`} alt="" className='movie-main-background'/>
             <div className='movie-main-container'>
-
+                <div className='movie-main-container-content'>
+                    <div className='movie-main-container-title'>{props.movie.title}</div>
+                    <div className='movie-main-container-text'>{props.movie.overview}</div>
+                    <div className='movie-main-container-buttons'>
+                        <Link className='btn-play'><i className="fa-solid fa-play"></i> Assistir</Link>
+                        <Link className='btn-info'><i className="fa-solid fa-circle-info"></i>Mais informações</Link>
+                    </div>
+                </div>
             </div>
             <div className='smoothie'></div>
         </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,9 @@ import './style.css';
 export default function Home(){
 
     const [filmes, setFilmes] = useState([]);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [topRated, setTopRated] = useState([]);
+    const [upcoming, setUpcoming] = useState([]);
     const [randomNumber, setRandomNumber] = useState(0);
 
     useEffect(() => {
@@ -22,13 +25,54 @@ export default function Home(){
             setFilmes(response.data.results.slice(0,10));
         }
 
+        async function loadPopularMovies(){
+            const response = await api.get("/movie/popular", {
+                params: {
+                    api_key: '6ad5e921680b9aa857c6fd1323650df7',
+                    language: 'pt-BR',
+                    page: 1
+                }
+            })
+
+            setPopularMovies(response.data.results.slice(0,10));
+
+        }
+
+        async function loadTopRated(){
+            const response = await api.get("/movie/top_rated", {
+                params: {
+                    api_key: '6ad5e921680b9aa857c6fd1323650df7',
+                    language: 'pt-BR',
+                    page: 1
+                }
+            })
+
+            setTopRated(response.data.results.slice(0, 10));
+        }
+
+        async function loadUpcoming(){
+            const response = await api.get("/movie/upcoming", {
+                params: {
+                    api_key: '6ad5e921680b9aa857c6fd1323650df7',
+                    language: 'pt-BR',
+                    page: 1
+                }
+            })
+
+            setUpcoming(response.data.results.slice(0, 10));
+        }
+
         function GenerateRandomNumber(list){
             const n = Math.floor(Math.random() * 10);
             setRandomNumber(n);
         }
 
         loadFilmes()
+        loadPopularMovies()
+        loadTopRated()
+        loadUpcoming()
         GenerateRandomNumber()
+
     }, [])
 
     
@@ -53,9 +97,29 @@ export default function Home(){
                 </div>
             </div>
             <div className='group-films'>
-                <div className='group-films-title'>Em cartaz</div>
+                <div className='group-films-title'>Mais populares</div>
                 <div className='group-films-cards'>
-                    {filmes.map(filme => {
+                    {popularMovies.map(filme => {
+                        return(
+                            <Film key={filme.id} film={filme}></Film>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className='group-films'>
+                <div className='group-films-title'>Bem avaliados</div>
+                <div className='group-films-cards'>
+                    {topRated.map(filme => {
+                        return(
+                            <Film key={filme.id} film={filme}></Film>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className='group-films'>
+                <div className='group-films-title'>Em breve</div>
+                <div className='group-films-cards'>
+                    {upcoming.map(filme => {
                         return(
                             <Film key={filme.id} film={filme}></Film>
                         )
